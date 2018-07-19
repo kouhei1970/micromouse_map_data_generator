e5b0e295bf5c5bc5496bf63ad6380ab803ac3140
Kouhei Ito <kouhei.ito@itolab-ktc.com>
2018-07-20 03:30:21 +0900

    modefy main function warning

--- map_data_generater.c
+++ map_data_generater.c
@@ -51,7 +51,7 @@ char smap[33][66]={
     // 0   1   2   3   4   5   6   7   8   9  10  11  12  13  14  15
 };
 
-void main(void){
+int main(void){
     short i,j;
     make_mapdata();
     for (i=0;i<16;i++){
